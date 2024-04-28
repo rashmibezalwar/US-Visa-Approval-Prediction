820775925c26f34f9b0d1762ad4df3056d3a64ea
rashmibezalwar <rashmibezalwar@gmail.com>
2024-04-28 22:58:30 +0100

    data added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rashmi",
+    author_email="rashmibezalwar@gmail.com",
+    packages=find_packages()
+)
--- template.py
+++ template.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+from pathlib import Path, path    
 
 project_name = "us_visa"
 
