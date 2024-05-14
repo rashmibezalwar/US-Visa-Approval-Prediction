f4d6d178ef3f5f54d26c9aef0cbe950a988c95d4
rashmibezalwar <rashmibezalwar@gmail.com>
2024-05-14 22:13:31 +0100

    EDA done

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
-from us_visa.logger import logging
+from us_visa.pipline.training_pipeline import TrainPipeline
 
 
-
-
-logging.info("Welcome to our custom log")
+pipline  = TrainPipeline()
+pipline.run_pipeline()
 
