a6441df24ad9a36964a79cd45a45d87ec4c62e20
rashmibezalwar <rashmibezalwar@gmail.com>
2024-05-17 11:24:57 +0100

    Changes done

--- demo.py
+++ demo.py
@@ -1,13 +1,1 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'us_visa')))
-
-
-
-
-from us_visa.pipline.training_pipeline import TrainPipeline
-
-
-pipline  = TrainPipeline()
-pipline.run_pipeline()
 
